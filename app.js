f570c754be395ffc3762fa80cc35b36ee1396b73
wallace-y <cameronwfinney@gmail.com>
2023-02-20 09:24:05 +0000

    adding skills-highlighting function based on project

--- app.js
+++ app.js
@@ -1,18 +1,75 @@
-// this one is jut to wait for the page to load
-document.addEventListener("DOMContentLoaded", () => {
-  const themeStylesheet = document.getElementById("theme");
-  const themeToggle = document.getElementById("theme-toggle");
-  themeToggle.addEventListener("click", () => {
-    // if it's light -> go dark
-    if (themeStylesheet.href.includes("light")) {
-      themeStylesheet.href = "css/dark-theme.css";
-      themeToggle.innerText = "Switch to light mode";
-    } else {
-      // if it's dark -> go light
-      themeStylesheet.href = "css/light-theme.css";
-      themeToggle.innerText = "Switch to dark mode";
-    }
-  });
-});
+// skill highlighting function
+
+function highlightSkills() {
+  // get the property of the accordion button element that shows which project is expanded 
+  const accordionOne = document.getElementById("headingOne").firstElementChild.getAttribute("aria-expanded");
+  const accordionTwo = document.getElementById("headingTwo").firstElementChild.getAttribute("aria-expanded");
+  const accordionThree = document.getElementById("headingThree").firstElementChild.getAttribute("aria-expanded");
+
+// get icon elements
+  const js = document.getElementById("javascriptIcon")
+  const html = document.getElementById("HTMLIcon")
+  const css = document.getElementById("CSSIcon")
+  const react = document.getElementById("ReactIcon")
+  const git = document.getElementById("gitIcon")
+  const bootstrap = document.getElementById("bootstrapIcon")
+  const node = document.getElementById("nodeIcon")
+  const python = document.getElementById("pythonIcon")
+  const ruby = document.getElementById("rubyIcon")
+  const sql = document.getElementById("SQLIcon")
+  const linux = document.getElementById("LinuxIcon")
+  const github = document.getElementById("gitHubIcon")
 
 
+
+
+
+  if (accordionOne === "true") {
+    // set live icons
+    js.setAttribute("class","col liveIcon")
+    html.setAttribute("class","col liveIcon")
+    css.setAttribute("class","col liveIcon")
+    react.setAttribute("class","col liveIcon")
+    git.setAttribute("class","col liveIcon")
+    bootstrap.setAttribute("class","col liveIcon")
+    node.setAttribute("class","col liveIcon")
+    linux.setAttribute("class","col liveIcon")
+    github.setAttribute("class","col liveIcon")
+    // set inactive icons
+    python.setAttribute("class","col inactiveIcon")
+    ruby.setAttribute("class","col inactiveIcon")
+    sql.setAttribute("class","col inactiveIcon")
+  } else if (accordionTwo === "true") {
+        // set live icons
+        // set inactive icons
+        js.setAttribute("class","col inactiveIcon")
+        html.setAttribute("class","col inactiveIcon")
+        css.setAttribute("class","col inactiveIcon")
+        react.setAttribute("class","col inactiveIcon")
+        git.setAttribute("class","col inactiveIcon")
+        bootstrap.setAttribute("class","col inactiveIcon")
+        node.setAttribute("class","col inactiveIcon")
+        linux.setAttribute("class","col inactiveIcon")
+        github.setAttribute("class","col inactiveIcon")
+        python.setAttribute("class","col inactiveIcon")
+        ruby.setAttribute("class","col inactiveIcon")
+        sql.setAttribute("class","col inactiveIcon")
+  } else if (accordionThree === "true") {
+        // set live icons
+        // set inactive icons
+        js.setAttribute("class","col inactiveIcon")
+        html.setAttribute("class","col inactiveIcon")
+        css.setAttribute("class","col inactiveIcon")
+        react.setAttribute("class","col inactiveIcon")
+        git.setAttribute("class","col inactiveIcon")
+        bootstrap.setAttribute("class","col inactiveIcon")
+        node.setAttribute("class","col inactiveIcon")
+        linux.setAttribute("class","col inactiveIcon")
+        github.setAttribute("class","col inactiveIcon")
+        python.setAttribute("class","col inactiveIcon")
+        ruby.setAttribute("class","col inactiveIcon")
+        sql.setAttribute("class","col inactiveIcon")  }
+
+}
+
+setInterval(highlightSkills,1000);
